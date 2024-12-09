21680ef8c230701af72d89d15700b02813ff3b52
a_adamovich <a.adamovich@just-ai.com>
2024-12-09 11:23:34 +0300

    experiments with api & module for extract data

---
+++ main/producer/extract_data_using_api.py
@@ -0,0 +1,46 @@
+import requests
+
+
+def fetch_github_commits(repo_owner: str, repo_name: str, access_token: str):
+    '''
+    Извлечение информации о коммитах с помошью Github API
+
+    Parameters
+    ----------
+    repo_owner: str
+        Владелец репозитория
+    repo_name: str
+        Название репозитория
+    access_token: str
+        Персональный токен доступа
+    '''
+    
+    url = f"https://api.github.com/repos/{repo_owner}/{repo_name}/commits"
+    headers = {
+        "Authorization": f"token {access_token}",
+        "Accept": "application/vnd.github.v3+json"
+    }
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        commits = response.json()
+        
+        # извлечение информации о коммитах
+        for commit in commits:
+            
+            sha = commit["sha"]
+            message = commit["commit"]["message"]
+            author = commit["commit"]["author"]["name"]
+            date = commit["commit"]["author"]["date"]
+
+            commit_data = {
+                "sha": sha,
+                "message": message,
+                "author": author,
+                "date": date
+            }
+            return commit_data
+    else:
+        print("Не удалось получить данные")
+        return None
